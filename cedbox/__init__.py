bd3dd437dab2578ec5a667a6273beeeeab7b93c6
wagner <cedric.sascha.wagner@outlook.de>
2025-07-22 13:39:27 +0200

    Bump version to 0.1.6 and update import for `MC_DICT` in `__init__.py`.

--- cedbox/__init__.py
+++ cedbox/__init__.py
@@ -7,8 +7,8 @@ from cedbox.inputs import (
     string_put, int_put, float_put, choice_put, 
     bool_put, file_put, date_put, mail_put
 )
-from cedbox.easymorse import EasyMorse, MORSE_CODE_DICT
+from cedbox.easymorse import EasyMorse, MC_DICT
 from cedbox.easywav import EasyWav
 from cedbox.easystream import EasyStream
 
-__version__ = "0.1.0"
+__version__ = "0.1.6"
